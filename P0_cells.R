1f79e8fb254bfad979fe3d4d274f7fa1858d478e
Dr. Laura Power <laura.power358@gmail.com>
2022-12-08 20:57:22 +0100

    Create P0_cells.R

---
+++ P0_cells.R
@@ -0,0 +1,25 @@
+# https://r-hyperspec.github.io/hyperSpec/
+
+#WiTec .spc file format - single spectra
+install.packages("hyperSpec")
+library ("hyperSpec")
+collapseHyperspec = function(path) {
+  data <- Sys.glob (path) 
+  data <- data [seq (1, length (data))] # import low wavenumber region only???
+  spc <- lapply (data, read.spc)
+  #read.txt.long imports long format ASCII files, i.e. one intensity value per row
+  length (spc)  
+  spc <- collapse (spc) #into a single hyperspec object
+  return (spc)
+}
+
+#cells on CaF2
+NC = collapseHyperspec("./Data/NC*.spc")
+PC = collapseHyperspec("./Data/PC*.spc")
+
+plot(mean(PC), col = "red")
+plot(mean(NC), col = "blue", add = TRUE)
+plot(PC, col = "red")
+plot(NC, col = "blue", add = TRUE)
+legend("top",c("NC","PC", "background"), lty = 1, col = c("blue", "red", "black"))
+title(main = "Mean P0 NC, PC, and background spectra")
